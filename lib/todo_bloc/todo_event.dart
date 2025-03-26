763533424614bbf4aba120b18bd356697456035d
hdsneshati <hadisneshati1384@gmail.com>
2025-03-26 10:20:01 +0330

    Update todo_event

--- lib/todo_bloc/todo_event.dart
+++ lib/todo_bloc/todo_event.dart
@@ -1,32 +1,31 @@
-
 part of 'todo_bloc.dart';
 
-abstract class TodoEvent extends Equatable {
-  const TodoEvent();
+abstract class TodoEvent extends Equatable{
+  const TodoEvent();//سازنده کلاس
 
-  @override
+   @override
   List<Object> get props => [];
 }
-
-class Todostarted extends TodoEvent{
-
-}
+class Todostarted extends TodoEvent{}
+///
 class  AddTodo extends TodoEvent{
-  final Todo todo;
-  const AddTodo(this.todo);
+///final در Dart به این معنی است که بعد از مقداردهی اولیه، نمی‌توان مقدار متغیر را تغییر داد.اما شیء درون آن مقادیر فیلدهای تودو قابل تغییر است
+final Todo todo;
+ const AddTodo(this.todo);//سازنده
   @override
   List<Object> get props => [];
-}
-class  RemoveTodo extends TodoEvent{
+ 
+ }
+ class  RemoveTodo extends TodoEvent{
   final Todo todo;
-  const RemoveTodo(this.todo);
+  const RemoveTodo(this.todo);//سازنده
   @override
   List<Object> get props => [];
 }
 
 class  AlterTodo extends TodoEvent{
   final int index;
-  const AlterTodo(this.index);
+  const AlterTodo(this.index);//سازنده
   @override
   List<Object> get props => [];
 }
