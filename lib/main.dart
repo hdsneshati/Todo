a51f9cab8f8ba45bf8e3b78c3f5f7158162bc679
hdsneshati <hadisneshati1384@gmail.com>
2025-03-27 00:47:47 +0330

    Update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_bloc_project/home.dart';
 import 'package:flutter_application_bloc_project/todo_bloc/todo_bloc.dart';
-import 'package:flutter_slidable/flutter_slidable.dart';
-import 'package:path_provider/path_provider.dart';
-import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:meta/meta.dart';
 import 'package:bloc/bloc.dart';
-void main() async{// تابع غیرهمزمان
- WidgetsFlutterBinding.ensureInitialized();//ین خط تضمین می‌کند که  ویجت‌های فلاتر مقداردهی اولیه شده‌اند
-  HydratedBloc.storage = await HydratedStorage.build(
-    storageDirectory:  HydratedStorageDirectory((await getTemporaryDirectory()).path),
+
+void main() {
+  // تابع غیرهمزمان
+
+  runApp(
+     BlocProvider<TodoBloc>(
+      create: (context) => TodoBloc(),
+       child:const MainApp()
+       ),
   );
-  runApp(const MainApp());
 }
+
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
@@ -27,18 +29,13 @@ class MainApp extends StatelessWidget {
         colorScheme: const ColorScheme.light(
           background: Colors.white,
           onBackground: Colors.black,
-          primary: Colors.yellowAccent,
+          primary: Color.fromARGB(255, 215, 122, 15),
           onPrimary: Colors.white,
           secondary: Colors.lightGreen,
           onSecondary: Colors.white,
-        ),        
+        ),
       ),
-      home: BlocProvider<TodoBloc>(
-        create: (context) => TodoBloc()..add(
-          Todostarted()
-          ),
-        child:const HomeScreen(),
-      ),
+      home:  const HomeScreen(),      
     );
   }
 }
