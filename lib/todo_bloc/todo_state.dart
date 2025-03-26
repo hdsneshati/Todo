cc38f3f09dfcbf2c41536570386f7e71859e4534
hdsneshati <hadisneshati1384@gmail.com>
2025-03-26 10:20:55 +0330

    Update todo_state

--- lib/todo_bloc/todo_state.dart
+++ lib/todo_bloc/todo_state.dart
@@ -1,10 +1,15 @@
 part of 'todo_bloc.dart';
 
+
+
+// مجموعه‌ای از مقادیر ثابت را تعریف می‌کند.
 enum TodoStatus {initial,loading,success,error}
+//وضعیت فعلی
 class TodoState extends Equatable  {
   final TodoStatus status;
   final List<Todo> todos;
 
+  //سازنده
   const TodoState({
     this.status=TodoStatus.initial,
 
@@ -12,15 +17,18 @@ class TodoState extends Equatable  {
     });
 
     TodoState copyWith({
+     
       TodoStatus? status,
       List<Todo>? todos,
     }) {
       return TodoState(
+         //اگر مقدار جدید داده نشود، مقدار قبلی حفظ خواهد شد
         status: status?? this.status,
         todos: todos?? this.todos,
       );
     }
-    @override
+     @override
+      @override
      factory TodoState.fromJson(Map<String, dynamic> json) {
       try {
         var listOfTodos=(json['todos'] as List<dynamic>).
@@ -46,3 +54,14 @@ class TodoState extends Equatable  {
     @override
     List<Object> get props => [ todos,status];
 }
+
+
+
+
+
+
+
+
+
+
+
